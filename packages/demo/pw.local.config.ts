05fdb8df1d81574c97fe1304fe7b8a6f9461e4e3
Dan <daniellehr@gmx.de>
2026-09-17 15:09:01 +0200

    Remove local Playwright override

--- packages/demo/pw.local.config.ts
+++
@@ -1,4 +0,0 @@
-import base from "./playwright.config";
-base.use = { ...(base.use ?? {}), launchOptions: { executablePath: "/opt/pw-browsers/chromium" } };
-base.reporter = "dot";
-export default base;
